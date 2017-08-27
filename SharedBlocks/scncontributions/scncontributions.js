2c0315d9bbdbe28fa75e6dfb9be630ce47b82076
viru48 <joinviru48@gmail.com>
2017-08-27 22:41:02 +1000

    scncontributions block js

--- SharedBlocks/scncontributions/scncontributions.js
+++ SharedBlocks/scncontributions/scncontributions.js
@@ -1,19 +1,19 @@
 sap.ui.define(['sap/uxap/BlockBase'], function (BlockBase) {
 	"use strict";
 
-	var GoalsBlock = BlockBase.extend("sap.uxap.sample.SharedBlocks.goals.GoalsBlock", {
+	var scncontributions = BlockBase.extend("sap.uxap.sample.SharedBlocks.scncontributions.scncontributions", {
 		metadata: {
 			views: {
 				Collapsed: {
-					viewName: "sap.uxap.sample.SharedBlocks.goals.GoalsBlock",
+					viewName: "sap.uxap.sample.SharedBlocks.goals.scncontributions",
 					type: "XML"
 				},
 				Expanded: {
-					viewName: "sap.uxap.sample.SharedBlocks.goals.GoalsBlock",
+					viewName: "sap.uxap.sample.SharedBlocks.goals.scncontributions",
 					type: "XML"
 				}
 			}
 		}
 	});
-	return GoalsBlock;
+	return scncontributions;
 }, true);
